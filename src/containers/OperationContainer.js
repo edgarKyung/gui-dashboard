4c704428e047ba9e0acb30009761660df3cacb49
edgar.kyung <kyung@ardentrobot.com>
2021-08-09 11:05:34 +0900

    feat: 로봇컨트롤 속도,각도 계산 추가

--- src/containers/OperationContainer.js
+++ src/containers/OperationContainer.js
@@ -1,14 +1,14 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import { OperationPage } from '../components/pages';
 import * as RobotApi from '../lib/Robot';
 
-const OperationContainer = ({children}) => {
+const OperationContainer = ({ children }) => {
 
   const point = {
-    name:'거점 1',
-    x:100,
-    y:200,
-    degree:0
+    name: '거점 1',
+    x: 100,
+    y: 200,
+    degree: 0
   };
   const pointList = [
     point,
@@ -20,7 +20,7 @@ const OperationContainer = ({children}) => {
   ]
 
   const handleClickPoint = async (point) => {
-    try{
+    try {
       console.log(point);
       await RobotApi.setPoint(point);
     } catch (err) {
@@ -28,19 +28,49 @@ const OperationContainer = ({children}) => {
     }
   }
 
+  // [TODO: 리팩토링]
+  // 터치중일 경우 주기적으로 명령 전송
+  // velocity : 속도 m/s (최대값 : 0.75)
+  // angle : -PI ~ PI 범위의 값, 직진일때 0, 오른손법칙에 따라 좌회전방향이 양수
+  let sendRequestFlag = false;
+  let velocity = 0;
+  let angle = 0;
+
+  async function requestMoveControl(ms) {
+    setTimeout(async () => {
+      try {
+        await RobotApi.setPoint({ velocity: velocity, angle: angle });
+      } catch (ex) {
+        console.error(ex);
+      } finally {
+        if (sendRequestFlag === true) {
+          requestMoveControl(ms);
+        }
+      }
+    }, ms);
+  }
+
   const handleRobotMoveStart = async (point) => {
-    try{
-      console.log(point);
-      // await RobotApi.setPoint(point);
+    try {
+      if (sendRequestFlag === false) {
+        sendRequestFlag = true;
+        requestMoveControl(30);
+      }
+      velocity = point.distance / 100;
+      angle = point.angle.radian - Math.PI / 2;
+      angle = angle < 0 ? angle + 2 * Math.PI : angle;
+      angle = angle > Math.PI ? angle - 2 * Math.PI : angle;
     } catch (err) {
       console.error(err)
     }
   }
 
   const handleRobotMoveEnd = async (point) => {
-    try{
-      console.log(point);
-      // await RobotApi.setPoint(point);
+    try {
+      velocity = 0;
+      angle = 0;
+      sendRequestFlag = false;
+      requestMoveControl(100);
     } catch (err) {
       console.error(err)
     }
@@ -58,7 +88,7 @@ const OperationContainer = ({children}) => {
   console.log(pointList);
   return (
     <Fragment>
-      <OperationPage 
+      <OperationPage
         pointList={pointList}
         onClickPoint={handleClickPoint}
         onClickRobotControl={handleClickRobotControl}
--- src/lib/Robot.js
+++ src/lib/Robot.js
@@ -2,20 +2,26 @@ import HttpClient from './HttpClient';
 
 const httpClient = new HttpClient();
 
+const sleepAsync = ms => new Promise(resolve => setTimeout(resolve, ms));
+
 export const setPoint = async (data) => {
-    try {
-        return await httpClient.post('/set/point', data);
-    } catch (err) {
-        console.error(err);
-        throw err;
-    }
+  try {
+    console.log(new Date().getTime(), data.velocity.toFixed(3), data.angle.toFixed(3));
+    await sleepAsync(30); // For Test
+    return true;
+    // return await httpClient.post('/set/point', data);
+
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 };
 
-export const robotControl = async (type,data) => {
-    try {
-        return await httpClient.post(`/set/${type}`, data);
-    } catch (err) {
-        console.error(err);
-        throw err;
-    }
+export const robotControl = async (type, data) => {
+  try {
+    return await httpClient.post(`/set/${type}`, data);
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 };
